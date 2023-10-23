086270bc9ca20614df896643327536d36fc66bc8
peterd2026 <145625793+peterd2026@users.noreply.github.com>
2023-10-23 10:54:23 +0800

    Update Pages.java

--- src/Pages.java
+++ src/Pages.java
@@ -3,11 +3,13 @@ public class Pages {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        
         System.out.print("Enter your age: ");
+        
         int age = scanner.nextInt();
-
+        
         int pagesToRead = 100 - age;
+        
         System.out.println("The minimal number of pages to read is: " + pagesToRead);
     }
 }
