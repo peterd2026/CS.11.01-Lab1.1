f5d36388ab6809924da77177adda61d6ffd888ea
peterdeng <peterd2026@student.cis.edu.hk>
2023-10-23 10:52:39 +0800

    lab 1.1

--- src/Dateable.java
+++ src/Dateable.java
@@ -1,6 +1,12 @@
+import java.util.Scanner;
 public class Dateable {
 
     public static void main(String[] args) {
-        
-    }
-}
+                Scanner scanner = new Scanner(System.in);
+
+                System.out.print("Enter your age: ");
+                int age = scanner.nextInt();
+                int youngestAge = (7 + (age / 2));
+                System.out.println("The age of the youngest person you can date is: " + youngestAge);
+            }
+        }
